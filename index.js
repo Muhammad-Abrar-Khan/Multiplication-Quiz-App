3d561f7fd2906f9672b7b3346a5f6efcdab829ee
Muhammad Abrar Khan <makabrarkhan110@gmail.com>
2023-11-12 18:16:57 +0500

    last commit

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const num1=Math.ceil(Math.random()*10);
-const num2=Math.ceil(Math.random()*10);
-const ques=document.getElementById("ques");
-ques.innerText=`What is ${num1} multiply by ${num2}?`;
-const correctans=num1*num2;
-const formel=document.getElementById("form");
-const inputel=document.getElementById("input");
-formel.addEventListener("submit", ()=>{
-    const userans= +inputel.value
-    console.log(userans, typeof userans);
-})
+document.addEventListener('DOMContentLoaded', function() {
+    const questionElement = document.getElementById('ques');
+    const answerElement = document.getElementById('input');
+    const formElement = document.getElementById('form');
+    const scoreElement = document.getElementById('score');
+    let score = 0; // Initialize score to 0
+
+    // Function to generate a new multiplication question
+    function generateQuestion() {
+        const num1 = Math.floor(Math.random() * 10) + 1;
+        const num2 = Math.floor(Math.random() * 10) + 1;
+        questionElement.textContent = `What is ${num1} multiply by ${num2}?`;
+        return num1 * num2;
+    }
+
+    // Function to handle the submission of the answer
+    function submitAnswer(event) {
+        event.preventDefault(); // Prevent form submission
+        const userAnswer = parseInt(answerElement.value);
+        if (userAnswer === correctAnswer) {
+            scoreElement.textContent = 'Correct!';
+            score++;
+        } else {
+            scoreElement.textContent = 'Incorrect. Try again!';
+            if (score > 0) {
+                score--;
+            }
+        }
+        scoreElement.textContent = `Score: ${score}`;
+        correctAnswer = generateQuestion();
+        answerElement.value = '';
+    }
+
+    let correctAnswer = generateQuestion();
+    scoreElement.textContent = `Score: ${score}`; // Update score display
+
+    formElement.addEventListener('submit', submitAnswer);
+});
